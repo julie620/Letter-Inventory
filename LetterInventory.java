65b1288c5d5686a55c90e1111cff1a2c7551b1b8
julie <juliana.chanel@gmail.com>
2024-10-09 08:50:11 -0700

    Starting place with empty methods

---
+++ LetterInventory.java
@@ -0,0 +1,44 @@
+//Letter Inventory
+
+public class LetterInventory {
+
+    public static void main(String[] args) {
+
+    }
+
+    /*public LetterInventory (String data) {
+
+    }*/
+
+    /*public int get (char letter) {
+        
+    }*/
+
+    /*public void set (char letter, int value) {
+
+    }*/
+
+    /*public void set (char letter, int value) {
+
+    }*/
+
+    /*public int size () {
+
+    }*/
+
+    /*public boolean isEmpty () {
+
+    }*/
+
+    /*public String toString () {
+
+    }*/
+
+    /*public LetterInventory add (LetterInventory other) {
+
+    }*/
+    
+    /*public LetterInventory subtract (LetterInventory other) {
+
+    }*/
+}
